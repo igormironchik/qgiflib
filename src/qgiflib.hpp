aa4a765c88011f45d9b1f53ea08b215873004788
Igor Mironchik <igor.mironchik@gmail.com>
2023-09-23 17:53:14 +0300

    Make a skelethon of the lbrary.

---
+++ src/qgiflib.hpp
@@ -0,0 +1,74 @@
+
+/*!
+	\file
+
+	\author Igor Mironchik (igor.mironchik at gmail dot com).
+
+	Copyright (c) 2023 Igor Mironchik
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#pragma once
+
+// Qt include.
+#include <QString>
+#include <QImage>
+
+
+namespace QGifLib {
+
+//
+// Gif
+//
+
+//! Gif file wrapper.
+class Gif final
+{
+public:
+	Gif() = default;
+	~Gif() = default;
+
+	//! Load GIF.
+	bool load(
+		//! Input file name.
+		const QString & fileName );
+	//! \return Count of frames.
+	qsizetype count() const;
+	//! \return Frame with given index (starting at 0).
+	QImage at(
+		//! Index of the requested frame (indexing starts with 0).
+		qsizetype idx ) const;
+
+	//! Write GIF from sequence of PNG files.
+	static bool write(
+		//! Output file name.
+		const QString & fileName,
+		//! Sequence of PNG file names.
+		const QStringList & pngFileNames,
+		//! Frames delay in milliseconds.
+		int delay,
+		//! Loop animated GIF forever?
+		bool loop );
+}; // class Gif
+
+} /* namespace QGifLib */
